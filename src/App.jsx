2d41a94112ace336e831d4938333c4e4ddd94525
Marcelo <marceloromero2016ar@gmail.com>
2023-10-16 22:00:24 +0200

    add feature description

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import { useState } from 'react'
 import SlideFilm from './components/slide'
-import WelcomeFilm from './components/Welcome'
+import Welcome from './components/Welcome'
 import films from './components/Films'
 import MovieOfDay from './components/Today';
+import './App.css'
 
 function App() {
 
   return (
     <>
-    <WelcomeFilm/>
-    <MovieOfDay />
-    <SlideFilm />
+      <Welcome />
+      <MovieOfDay />
+      <SlideFilm />
     </>
   )
 }
--- src/components/slide.jsx
+++ src/components/slide.jsx
@@ -3,7 +3,7 @@ import './slide.css'
 
 function SlideFilm(params) {
     const [currentIndex, setCurrentIndex] = useState(2);
-    
+
     const prevSlide = () => {
       setCurrentIndex((prevIndex) => (prevIndex === 0 ? 0 : prevIndex - 1));
     };
@@ -12,7 +12,7 @@ function SlideFilm(params) {
       setCurrentIndex((prevIndex) => (prevIndex === 4 ? 4 : prevIndex + 1));
     };
     return(
-        <div>
+        <>
             <div className="blockcomponent">
                 <div className="containercarrousel">
                     <div className="carousel">
@@ -31,16 +31,16 @@ function SlideFilm(params) {
                 </div>
                 <div className="blockdescription">
                     <div className="contain-card">
-                        <div className="card"></div>
-                        <div className="contain-description"></div>
+                        <div className="card"><img src="src/assets/Inception.jpg" alt="" /></div>
+                        <div className="contain-description"><h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, quidem.</h3></div>
                     </div>
                     <div className="contain-resume">
-                        <h3>titre du film</h3>
-                        <article></article>
+                        <h2>Titre du Film</h2>
+                        <article>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt facere aliquam veniam ipsam itaque aperiam veritatis iure totam sunt ut culpa, dignissimos earum obcaecati enim eum accusamus ea eos impedit beatae asperiores alias corrupti ratione sit quae? Aliquam excepturi nemo sunt obcaecati ut alias eos repellat quam itaque voluptates quisquam voluptatum velit pariatur laboriosam quibusdam enim eveniet, debitis, temporibus ullam.</article>
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
 export default SlideFilm
