e18e0628bd836ac3e94ba03d07ee9069d1f45442
Adri-bis <adrien.ma.16@hotmail.fr>
2023-10-17 10:54:07 +0200

    add noel theme, finish css

--- src/components/Today.jsx
+++ src/components/Today.jsx
@@ -2,25 +2,6 @@ import "./TodayStyle.css";
 import { useState } from "react";
 import films from "./Films";
 
-// const film = [
-
-//     {
-//         name: "Sawxxxxxxxxhsghsdfhsdfhrdfgr",
-//         genre: "horror",
-//         imgsrc: "src/assets/Saw.jpg"
-//     },
-//     {
-//         name: "Ça",
-//         genre: "horror",
-//         imgsrc: "src/assets/Sa.jpg"
-//     },
-//     {
-//         name: "Barbie",
-//         genre: "horror",
-//         imgsrc: "src/assets/Barbie.jpg"
-//     }
-// ]
-
 function MovieOfDay() {
 
     const [Filmei, setFilmei] = useState(0);
@@ -31,34 +12,31 @@ function MovieOfDay() {
     const handleClickN = () => {
         setFilmei(Filmei + 1)
     }
-    const film = [
-        {
-            nom: "Ça",
-            categorie: "Horreur",
-            note: "7.4/10",
-            resume: "Un groupe d'enfants affronte leurs peurs et un sinistre clown, Pennywise, dans la petite ville de Derry, Maine.",
-            image: "src/assets/It.jpg",
-            avis: "Ça est un film d'horreur terrifiant. L'interprétation de Bill Skarsgård en Pennywise est cauchemardesque, et l'atmosphère est oppressante."
-        },
-        {
-            nom: "The Shining",
-            categorie: "Horreur",
-            note: "8.4/10",
-            resume: "Une famille se retrouve isolée dans un hôtel en montagne pendant l'hiver, mais des forces surnaturelles commencent à les tourmenter.",
-            image: "src/assets/The Shining.jpg",
-            avis: "The Shining est un classique de l'horreur. La tension monte lentement, et Jack Nicholson livre une performance mémorable en devenant de plus en plus fou."
-        },
-    ]
-    const filmN = film[Filmei]
 
+    const horroFilm = (cat) => {
+
+        return films.filter((el) => {
+            return el.categorie === cat
+        })
+    }
+
+    const filmN = horroFilm("Animation")[Filmei]
+
+    const Hotext =
+        <div>Bienvenue a toi, passionné de cinéma!  En cette période d'Halloween, quoi de mieux que de se mettre dans l'ambiance avec des films?
+            d'HORREUR! Voici notre recommandation du moment!</div>;
+    const Notext =
+        < div > Noyeux Joël !  (Ce référé à Quantin, notre CEO, pour toute plainte envers notre humour.Merci). Quoi de mieux qu'un bon film d'animation, au coin de l'incendie avec un chocolat chaud et de la neige tombant par la fenêtre!
+            Nous vous présentons notre recommandation pour Noël!
+        </div >
+    const musi = <audio controls autoPlay ><source src="src\assets\All-is-fine.mp3" type="audio/mp3"></source></audio>
     return (
-        <div className="Tblock"><div className="Texteblockl" >Bienvenu à toi fan de film! En cette période de Hallowen quoi de mieux que de ce mettre dans l'ambiance avec des films
-            d'HORREUR!! Voici notre recommandation du moment!</div>
+        <div className="TNoblock"><div className="Texteblockl" >{Notext}{musi}</div>
             <div ><figure className="imgTblock"><img className="imgT" src={filmN.image}></img>
                 <div className="BlockB">
                     {Filmei > 0 ? <button className="ButonT" onClick={handleClickP}>précédent</button> : null}
                     <h1 className="MovieN">{filmN.nom}</h1>
-                    {Filmei < film.length - 1 ? <button className="ButonT" onClick={handleClickN}>next</button> : null}
+                    {Filmei < horroFilm("Animation").length - 1 ? <button className="ButonT" onClick={handleClickN}>next</button> : null}
                 </div></figure></div>
         </div>
 
